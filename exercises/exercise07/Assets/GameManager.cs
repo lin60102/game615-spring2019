dc8ddbdf3a1e149f7a94e015aa6dc8e546df848b
CHAO CHENG LIN(Ken) <38403949+lin60102@users.noreply.github.com>
2019-03-31 09:46:23 -0400

    fix
    This reverts commit bb903f7ae776b5bb53aa14c6dcd38607523ab88c.

---
+++ exercises/exercise07/Assets/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public Image fadePanelImg;
+    public Animator Anim_player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fadeIn());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator fadeIn()
+    {
+        //In this function, we will slowly lower the 'alpha' (transparency) value of the giant
+        //image's color to achieve a 'fade in' effect (fading from black to clear).
+        while (fadePanelImg.color.a > 0)
+        {
+            float newAlpha = fadePanelImg.color.a - 0.5f * Time.deltaTime;
+            fadePanelImg.color = new Color(0, 0, 0, newAlpha);
+
+            //This line will end the function for this round of Unity's update cycle (at time=n)
+            yield return null;
+            //This is where the Unity update cycle will enter at time=n+1
+        }
+
+        //Once we're done fading, we can set the fade panel to inactive. We do this to avoid
+        //the panel 'catching' clicks. Sometimes an invisible panel can make it so our click
+        //events aren't recognized as expected.
+        fadePanelImg.gameObject.SetActive(false);
+        Anim_player.SetBool("start", true);
+        //Start the drop the cube scene
+
+    }
+
+}
---
+++ exercises/exercise07/Assets/move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class move : MonoBehaviour
+{
+    public GameObject mariotxt;
+    public GameObject eat;
+    public GameObject see;
+    public Animator Anim_player;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        mariotxt = GameObject.Find("mariotext");
+        eat = GameObject.Find("eat");
+        see = GameObject.Find("see");
+        Anim_player = GetComponent<Animator>();
+        mariotxt.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+    IEnumerator OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        switch (other.gameObject.name)
+        {
+            case "see":
+                mariotxt.SetActive(true);
+                yield return new WaitForSeconds(0.5f);
+                see.SetActive(false);
+                mariotxt.SetActive(false);
+
+                break;
+            case "eat":
+                Anim_player.SetBool("eat", true);
+                eat.SetActive(false);
+                break;
+
+        }
+    }
+}
